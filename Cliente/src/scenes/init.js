8c51f78e8a1719e3c40483d2298bb27f5f179609
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2020-11-19 16:13:03 -0500

    order

--- Cliente/src/scenes/init.js
+++ Cliente/src/scenes/init.js
@@ -11,9 +11,7 @@ const config = {
     physics: {
         default: "arcade",
         arcade:{
-            gravity: 0,
-            debug: true
-
+            gravity: 0
         }
     },
     scene:[
