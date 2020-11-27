ba168f7788793a685d57233d5c5ab16e49e1613a
kovrovksvk@gmail.com <kovrovksvk@gmail.com>
2020-11-26 19:09:03 -0500

    mas integracion

--- Servidor/socket.js
+++ Servidor/socket.js
@@ -8,6 +8,7 @@ app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/assets', express.static(__dirname + '/assets'));
 
+app.use(express.static(__dirname + '/Cliente'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
